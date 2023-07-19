94d08c12d42efca96c30c76674bc749944149df6
Anibal Corral Serrano <anibal.corral@gmail.com>
2023-07-19 11:28:04 -0400

    Strings added

---
+++ src/06-strings.ts
@@ -0,0 +1,14 @@
+(()=>{
+let productTitle = 'Ipods'
+let productPrice = 100;
+productTitle = 'Iphone'
+console.log('productTitle', productTitle);
+const productDescription = "I'm blab lba"
+console.log('productDescription', productDescription);
+const summary = `title: ${productTitle}
+description: ${productDescription}
+price: ${productPrice}`
+console.log('summary', summary);
+
+
+})();
