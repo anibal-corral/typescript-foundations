33900fc84de2d12f91f58b7cae0a1a2d4fc1d21d
Anibal Corral Serrano <anibal.corral@gmail.com>
2023-07-19 11:18:00 -0400

    Working with numbers@

---
+++ src/04-numbers.ts
@@ -0,0 +1,30 @@
+(()=>{
+  let productPrice = 100;
+  productPrice =12;
+  console.log('Product price : ', productPrice);
+  let customerAge:number =28;
+  customerAge = customerAge + 1;
+  console.log('Customer age: ', customerAge);
+
+  let productInStock:number;
+  console.log('Product in stock: ', productInStock);
+  if(productInStock>10){
+    console.log('is greater');
+  }
+  let discount = parseInt('100');
+  console.log('Discount', discount);
+if(discount<=200){
+  console.log('apply discount')
+}else{
+  console.log('No apply discount');
+
+}
+
+let hex = 0xfff;
+console.log('hex', hex);
+let bin = 0b10101;
+console.log('bin', bin);
+
+const myNumber:Number = 10;//this is a class Number not a root number
+
+})();
