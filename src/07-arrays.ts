c2d8c8e7640e2f715d1cd261d29709d45a490e23
Anibal Corral Serrano <anibal.corral@gmail.com>
2023-07-19 11:37:02 -0400

    Arrays added

---
+++ src/07-arrays.ts
@@ -0,0 +1,15 @@
+(()=>{
+  const prices=[1,2,3,4,2,3];
+  prices.push(1223)
+let products = ['hola', true];
+products.push(false);
+let mixed: (number | string | boolean)[]=['hola', true];
+let numbers = [1,2,3,3,3,3,3];
+let numbersMul=numbers.map(item=>item*2.5);
+numbers.map(item => item*4)
+
+
+console.log(numbersMul);
+
+
+  })();
