2a008edab0c17e3f0607d0c90f878829451b6123
Anibal Corral Serrano <anibal.corral@gmail.com>
2023-07-19 14:34:06 -0400

    Parameters with typed objects

---
+++ src/15-objects.ts
@@ -0,0 +1,26 @@
+(()=>
+{
+  type Sizes= 'S' | 'M' | 'L' | 'XS';
+  type Product = {title:string, createdAt:Date,stock:number,size?:Sizes};
+
+  const products:Product[] = [];
+  const addProduct = (data:Product)=>{
+    products.push(data);
+  }
+
+  addProduct({
+    title:'Product 1',
+    createdAt: new Date(),
+    stock:12
+  });
+  addProduct({
+    title:'Product 2',
+    createdAt: new Date(),
+    stock:100
+  });
+  console.log(products);
+
+
+
+}
+)();
